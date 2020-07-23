efd7e7cdd2bfeaf4cb1541398a882fa823ae970a
Eduardo Folly <edufolly@gmail.com>
2020-07-23 21:43:04 +0000

    First execution test.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Prometheus Install
 
 ## Script to install Prometheus on Ubuntu 18.04
+
+`# curl -fsSL https://raw.githubusercontent.com/edufolly/prometheus_install/master/install.sh | bash`
--- install.sh
+++ install.sh
@@ -24,11 +24,46 @@ tar xzf prometheus-$PROMETHEUS_VERSION.linux-amd64.tar.gz
 
 tar xzf node_exporter-$NODE_EXPORTER_VERSION.linux-amd64.tar.gz
 
+wget -q https://raw.githubusercontent.com/edufolly/prometheus_install/master/node_exporter.service
 
+wget -q https://raw.githubusercontent.com/edufolly/prometheus_install/master/prometheus.service
 
+wget -q https://raw.githubusercontent.com/edufolly/prometheus_install/master/prometheus.yml
 
+useradd -M -r -s /bin/false prometheus
 
+mkdir /etc/prometheus /var/lib/prometheus
 
-# cd ~
+cp prometheus-$PROMETHEUS_VERSION.linux-amd64/{prometheus,promtool} /usr/local/bin/
+
+chown prometheus:prometheus /usr/local/bin/{prometheus,promtool}
+
+cp -r prometheus-$PROMETHEUS_VERSION.linux-amd64/{consoles,console_libraries} /etc/prometheus/
+
+cp prometheus.yml /etc/prometheus/prometheus.yml
+
+chown -R prometheus:prometheus /etc/prometheus
+
+chown prometheus:prometheus /var/lib/prometheus
+
+cp prometheus.service /etc/systemd/system/prometheus.service
+
+cp node_exporter-$NODE_EXPORTER_VERSION.linux-amd64/node_exporter /usr/local/bin/
+
+chown prometheus:prometheus /usr/local/bin/node_exporter
+
+cp node_exporter.service /etc/systemd/system/node_exporter.service
+
+systemctl daemon-reload
+
+systemctl enable node_exporter
+
+systemctl enable prometheus
+
+systemctl start node_exporter
+
+systemctl start prometheus
+
+cd ~
 
 # rm -rf $WIP
